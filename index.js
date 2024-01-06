d4adfc352f290f30c7f5ee886aae968f9454f898
georgetheoracle <uche1694@student.babcock.edu.ng>
2024-01-06 19:44:26 +0100

    test

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const countdownElement = document.getElementById("countdown");
+const hamburger = document.querySelector('.fa-bars')
+const navMenu = document.querySelector('.container')
+const navLink = document.querySelectorAll('.nav-link')
+
+// HAMBURGER MENU
+hamburger.addEventListener('click', ()=>{
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+navLink.forEach(x => x.addEventListener('click', ()=>{
+    hamburger.classList.remove('active')
+    navMenu.classList.remove('active')
+}))
 // Set the date we're counting down to
 const countDownDate = new Date("Jan 28, 2024 00:00:00").getTime();
 
